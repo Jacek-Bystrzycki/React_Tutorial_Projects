87b7cac00706fc992c7f602f912b9f1747fbdac1
Jacek Bystrzycki <bystrzyckijacek@gmail.com>
2022-04-17 07:22:32 +0200

    API and Extrnral libraries Projects - init commit

---
+++ API and External Libraries/Markdown preview/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+function App() {
+
+const [markdown, setMarkdown] = useState("## markdown preview");
+
+  return <main>
+    <section className="markdown">
+      <textarea className='input' value={markdown} onChange={(ev)=>{setMarkdown(ev.target.value)}}/>
+      <article className="result">
+        <ReactMarkdown>
+          {markdown}
+        </ReactMarkdown>
+      </article>
+    </section>
+  </main>
+};
+
+export default App;
